f076fbb75637205db66d91e40deb4c8fb324a000
iSmartboiUjjwal <122801676+iSmartboiUjjwal@users.noreply.github.com>
2023-03-03 12:12:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Elsa
 fi
-cd /Elsa
+cd /Autofilter-Shortner
 pip3 install -U -r requirements.txt
 echo "𝙎𝙩𝙖𝙧𝙩𝙞𝙣𝙜 𝙮𝙤𝙪𝙧 𝘽𝙤𝙩 ....🧞‍♂️"
 python3 bot.py
